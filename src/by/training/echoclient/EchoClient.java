cfdc869880a2f37abd5c3e64c99a77f1e5d16474
Deazushka <deazushka@gmail.com>
2018-08-16 16:27:02 +0300

    Initial commit

---
+++ src/by/training/echoclient/EchoClient.java
@@ -0,0 +1,20 @@
+package by.training.echoclient;
+
+import java.io.*;
+import java.net.Socket;
+
+class EchoClient {
+
+    public static void main(String[] args) throws IOException {
+        String message = args[0]; //{\"name\":\"Tom\",\"message\":\"Hello,server\"}
+        String hostName = "localhost";
+        int portNumber = 8080;
+        try (Socket echoSocket = new Socket(hostName, portNumber);
+             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))) {
+            out.println(message);
+            String answer = in.readLine();
+            System.out.println(answer);
+        }
+    }
+}
